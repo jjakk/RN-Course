2d40466d3a9aa62f1ffa71d373e861d2648cb6ac
Jak <jaksawesome10@gmail.com>
2021-04-02 12:28:28 -0400

    counter demo

--- rn-starter/src/components/ImageDetail.js
+++ rn-starter/src/components/ImageDetail.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image, TextPropTypes, ShadowPropTypesIOS } from 'react-native';
 
-const ImageDetail = (props) => {
-    return <Text>{props.title}</Text>;
+const ImageDetail = ({title, imageSource, imageScore}) => {
+    return (
+        <View>
+            <Image source={imageSource}/>
+            <Text>{title}</Text>
+            <Text>Image score - {imageScore}</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({});
--- rn-starter/src/screens/ImageScreen.js
+++ rn-starter/src/screens/ImageScreen.js
@@ -5,9 +5,21 @@ import ImageDetail from '../components/ImageDetail';
 const ImageScreen = () => {
     return (
         <View>
-            <ImageDetail title='Forest' />
-            <ImageDetail title='Beach' />
-            <ImageDetail title='Mountain' />
+            <ImageDetail
+                title='Forest'
+                imageSource={require('../../assets/forest.jpg')}
+                imageScore={5}
+            />
+            <ImageDetail
+                title='Beach'
+                imageSource={require('../../assets/beach.jpg')}
+                imageScore={8}
+            />
+            <ImageDetail
+                title='Mountain'
+                imageSource={require('../../assets/mountain.jpg')}
+                imageScore={6}
+            />
         </View>
     );
 };
