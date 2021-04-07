6a675bee38d8098d3bfc7fdd763971c28009b78f
Jak <jaksawesome10@gmail.com>
2021-04-06 20:05:08 -0400

    green and blue

--- rn-starter/src/screens/SquareScreen.js
+++ rn-starter/src/screens/SquareScreen.js
@@ -6,7 +6,7 @@ const SquareScreen = () => {
     const [red, setRed] = useState(0);
     const [green, setGreen] = useState(0);
     const [blue, setBlue] = useState(0);
-    console.log(red);
+    console.log(`Red: ${red}, Blue: ${blue}, Green: ${green}`);
 
     return (
         <View>
@@ -15,8 +15,16 @@ const SquareScreen = () => {
                 onDecrease={() => setRed(red-1)}
                 color='Red'
             />
-            <ColorCounter color='Green' />
-            <ColorCounter color='Blue' />
+            <ColorCounter
+                onIncrease={() => setGreen(green+1)}
+                onDecrease={() => setGreen(green-1)}
+                color='Green'
+            />
+            <ColorCounter
+                onIncrease={() => setBlue(blue+1)}
+                onDecrease={() => setBlue(blue-1)}
+                color='Blue'
+            />
         </View>
     );
 }
