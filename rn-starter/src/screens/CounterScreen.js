b46c89212a694844c5b3d6f6685f4541af10f6ce
Jak <jaksawesome10@gmail.com>
2021-04-11 13:36:07 -0400

    using reducer instead of state

--- rn-starter/src/screens/CounterScreen.js
+++ rn-starter/src/screens/CounterScreen.js
@@ -1,24 +1,34 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'change_count':
+            return {...state, count: state.count + action.payload};
+        default:
+            return state;
+    }
+};
+
 const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {count: 0});
+    const { count } = state;
 
     return (
         <View>
             <Button
                 title='Increase'
                 onPress={() => {
-                    setCounter(counter + 1);
+                    dispatch({type: 'change_count', payload: 1});
                 }}
             />
             <Button
                 title='Decrease'
                 onPress={() => {
-                    setCounter(counter - 1);
+                    dispatch({type: 'change_count', payload: -1});
                 }}
             />
-            <Text>Currnet Count: {counter}</Text>
+            <Text>Currnet Count: {count}</Text>
         </View>
     );
 };
