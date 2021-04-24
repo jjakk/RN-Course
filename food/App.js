036c415c62fcf1b86352a43c7cc67b409a64cbb2
Jak <jaksawesome10@gmail.com>
2021-04-24 10:40:05 -0400

    This little "i" costed me like 3 days of frustration

--- food/App.js
+++ food/App.js
@@ -1,4 +1,4 @@
-import { createAppContainer } from 'react-navigaton';
+import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import SearchScreen from './src/screens/SearchScreen';
 
