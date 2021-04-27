d2e79c41297a01624bfec6c607c6c0c2da45a543
Jak <jaksawesome10@gmail.com>
2021-04-27 11:18:11 -0400

    results list component

--- food/src/screens/SearchScreen.js
+++ food/src/screens/SearchScreen.js
@@ -1,38 +1,25 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar.js';
-import yelp from '../api/yelp';
+import useResults from '../hooks/useResults';
+import ResultsList from '../components/ResultsList'
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-    
-    const searchApi = async () => {
-        try{
-            const response = await yelp.get('/search', {
-                params: {
-                    limit: 50,
-                    term,
-                    location: 'stamford ct'
-                }
-            });
-            setResults(response.data.businesses);
-        }
-        catch(err){
-            setErrorMessage('Something went wrong');
-        }
-    };
+    const [searchApi, results, errorMessage] = useResults();
 
     return (
         <View>
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
-                onTermSubmit={searchApi}
+                onTermSubmit={() => searchApi(term)}
             />
             {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text> We have found {results.length} results</Text>
+            <ResultsList title='Cost Effective' />
+            <ResultsList title='Bit Pricier' />
+            <ResultsList title='Big Spender' />
         </View>
     );
 };
