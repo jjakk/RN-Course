ef18016abe3616a8cdebf79ba26f19fb51ac91f0
Jak <jaksawesome10@gmail.com>
2021-05-08 15:18:47 -0400

    minor mind blowing revelations

--- blog/App.js
+++ blog/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
@@ -11,5 +12,9 @@ const navigator = createStackNavigator({
   }
 });
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />;
+};
 
