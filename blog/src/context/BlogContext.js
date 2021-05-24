faf4ad9925a02b7f02ace62c2f155be1b3a06658
Jak <jaksawesome10@gmail.com>
2021-05-24 12:50:16 -0400

    blog pressability

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -3,7 +3,16 @@ import createDataContext from './createDataContext';
 const blogReducer = (state, action) => {
     switch(action.type){
         case 'add_blogpost':
-            return [...state, { title: `Blog Post #${state.length + 1}` }];
+            return [...state, {
+                /*
+                ID should be unique, but for the sake of time
+                we're just going to set it to a random number
+                */
+                id: Math.floor(Math.random()*99999),
+                title: `Blog Post #${state.length + 1}`
+            }];
+        case 'delete_blogpost':
+            return state.filter((blogPost) => blogPost.id !== action.payload);
         default:
             return state;
     }
@@ -15,9 +24,15 @@ const addBlogPost = (dispatch) => {
     };
 };
 
+const deleteBlogPost = (dispatch) => {
+    return (id) => {
+        dispatch({ type: 'delete_blogpost', payload: id });
+    };
+}
+
 export const { Context, Provider } = createDataContext(
     blogReducer,
-    { addBlogPost },
+    { addBlogPost, deleteBlogPost },
     []
 );
 
