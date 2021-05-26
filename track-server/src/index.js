97d0e0c9b83dd7b00aba20c5110dce8631275ced
Jak <jaksawesome10@gmail.com>
2021-05-26 12:31:01 -0400

    track server

---
+++ track-server/src/index.js
@@ -0,0 +1,34 @@
+require('./models/User');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const app = express();
+require('dotenv').config();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoURI = `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.0dzyx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+
+mongoose.connection.on('error', () => {
+    console.error('Error connecting to mongo', err);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hi there!');
+});
+
+app.listen(3000, () => {
+    console.log('listening on port 3000');
+});
+
