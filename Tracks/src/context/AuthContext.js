06d4fc673ce825b436f18f110d00a77b7584e41e
Jak <jaksawesome10@gmail.com>
2021-06-02 12:48:26 -0400

    storing token

--- Tracks/src/context/AuthContext.js
+++ Tracks/src/context/AuthContext.js
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-community/async-storage";
 import createDataContext from './createDataContext';
 import trackerApi from '../api/tracker';
 
@@ -5,21 +6,22 @@ const authReducer = (state, action) => {
     switch(action.type){
         case 'add_error':
             return { ...state, errorMessage: action.payload };
+        case 'signup':
+            return { errorMessage: '', token: action.payload };
         default:
             return state;
     }
 };
 
-const signup = (dispatch) => {
-    return async ({ email, password }) => {
-        try{
-            const response = await trackerApi.post('/signup', { email, password });
-            console.log(response.data);
-        }
-        catch(err){
-            dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' });
-        }
-    };
+const signup = (dispatch) => async ({ email, password }) => {
+    try{
+        const response = await trackerApi.post('/signup', { email, password });
+        await AsyncStorage.setItem('token', response.data.token);
+        dispatch({ type: 'signup', payload: response.data.token });
+    }
+    catch(err){
+        dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' });
+    }
 };
 
 const signin = (dispatch) => {
