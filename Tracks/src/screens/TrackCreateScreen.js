bdebd1801fb50f0350821bfd38bb558df92e8e69
Jak <jaksawesome10@gmail.com>
2021-06-21 13:13:32 -0400

    mock location

--- Tracks/src/screens/TrackCreateScreen.js
+++ Tracks/src/screens/TrackCreateScreen.js
@@ -1,8 +1,9 @@
+import '../_mockLocation';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { requestForegroundPermissionsAsync } from 'expo-location';
+import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
@@ -10,7 +11,14 @@ const TrackCreateScreen = () => {
     
     const startWatching = async () => {
         try {
-            const { granted } = await requestPermissionsAsync();
+            const { granted } = await requestForegroundPermissionsAsync();
+            await watchPositionAsync({
+                accuracy: Accuracy.BestForNavigation,
+                timeInterval: 1000,
+                distanceInterval: 10
+            }, (location) => {
+                console.log(location);
+            });
             if (!granted) {
                 throw new Error('Location permission not granted');
             }
