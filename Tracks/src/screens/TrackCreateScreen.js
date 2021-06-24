a20150d54100153f8314a8620f9d6c59fd2fd808
Jak <jaksawesome10@gmail.com>
2021-06-24 15:50:45 -0400

    isFocused

--- Tracks/src/screens/TrackCreateScreen.js
+++ Tracks/src/screens/TrackCreateScreen.js
@@ -3,13 +3,13 @@ import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { withNavigationFocus } from 'react-navigation'
 import Map from '../components/Map';
 import { Context as LocationContext } from '../context/LocationContext';
 import useLocation from '../hooks/useLocation';
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused }) => {
     const { addLocation } = useContext(LocationContext);
-
     const [err] = useLocation(addLocation);
 
     return (
@@ -23,4 +23,4 @@ const TrackCreateScreen = () => {
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
