c137a5fc0d43dfb2b82af15c7ee4cc85b6b4cbb2
Jak <jaksawesome10@gmail.com>
2021-06-24 21:20:00 -0400

    recording context

--- Tracks/src/components/Map.js
+++ Tracks/src/components/Map.js
@@ -2,12 +2,13 @@ import React, { useContext } from 'react';
 import { Text, StyleSheet, ActivityIndicator } from 'react-native';
 import MapView, { Polyline, Circle } from 'react-native-maps';
 import { Context as LocationContext } from '../context/LocationContext';
+import Spacer from './Spacer';
 
 const Map = () => {
     const { state: { currentLocation } } = useContext(LocationContext);
 
     if(!currentLocation){
-        return <ActivityIndicator size='large' style={{ marginTop: 200 }} />;
+        return <ActivityIndicator size='large' color="#0000ff" style={{ marginTop: 200 }} />;
     }
 
     initialLocation = {
@@ -16,21 +17,23 @@ const Map = () => {
     };
 
     return (
-        <MapView
-            style={styles.map}
-            initialRegion={{
-                ...initialLocation,
-                latitudeDelta: 0.01,
-                longitudeDelta: 0.01,
-            }}
-        >
-            <Circle
-                center={currentLocation.coords}
-                radius={30}
-                strokeColor='rgba(158, 158, 255, 1.0)'
-                fillColor='rgba(158, 158, 255, 0.3)'
-            />
-        </MapView>
+        <Spacer>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    ...initialLocation,
+                    latitudeDelta: 0.01,
+                    longitudeDelta: 0.01,
+                }}
+            >
+                <Circle
+                    center={currentLocation.coords}
+                    radius={30}
+                    strokeColor='rgba(158, 158, 255, 1.0)'
+                    fillColor='rgba(158, 158, 255, 0.3)'
+                />
+            </MapView>
+        </Spacer>
     );
 }
 
--- Tracks/src/screens/TrackCreateScreen.js
+++ Tracks/src/screens/TrackCreateScreen.js
@@ -3,19 +3,24 @@ import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { withNavigationFocus } from 'react-navigation'
+import { withNavigationFocus } from 'react-navigation';
+import Spacer from '../components/Spacer';
 import Map from '../components/Map';
 import { Context as LocationContext } from '../context/LocationContext';
 import useLocation from '../hooks/useLocation';
 import TrackForm from '../components/TrackForm';
 
 const TrackCreateScreen = ({ isFocused }) => {
-    const { addLocation } = useContext(LocationContext);
-    const [err] = useLocation(isFocused, addLocation);
+    const { state, addLocation } = useContext(LocationContext);
+    const [err] = useLocation(isFocused, (location) => {
+        addLocation(location, state.recording);
+    });
 
     return (
         <SafeAreaView forceInset={{top: 'always'}}>
-            <Text h2>Create a Track</Text>
+            <Spacer>
+                <Text h2>Create a Track</Text>
+            </Spacer>
             <Map />
             {err ? <Text>Please enable location services</Text> : null}
             <TrackForm />
