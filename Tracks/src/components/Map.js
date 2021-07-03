758d0c1189ec4365519cc41f604aaa7c544ed735
John Khachian <jaksawesome10@gmail.com>
2021-07-03 10:37:38 -0400

    Bring back the polyline

--- Tracks/src/components/Map.js
+++ Tracks/src/components/Map.js
@@ -5,13 +5,13 @@ import { Context as LocationContext } from '../context/LocationContext';
 import Spacer from './Spacer';
 
 const Map = () => {
-    const { state: { currentLocation } } = useContext(LocationContext);
+    const { state: { currentLocation, locations } } = useContext(LocationContext);
 
     if(!currentLocation){
         return <ActivityIndicator size='large' color="#0000ff" style={{ marginTop: 200 }} />;
     }
 
-    initialLocation = {
+    const initialLocation = {
         longitude: -122.0312186,
         latitude: 37.33233141,
     };
@@ -32,6 +32,7 @@ const Map = () => {
                     strokeColor='rgba(158, 158, 255, 1.0)'
                     fillColor='rgba(158, 158, 255, 0.3)'
                 />
+                <Polyline coordinates={locations.map(loc => loc.coords)} />
             </MapView>
         </Spacer>
     );
